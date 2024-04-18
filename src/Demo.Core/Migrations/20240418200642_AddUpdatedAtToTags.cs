7f992ce66e8c1cd80c2941d8d25ebc06806ea6f0
IonutStef <Dinu.Ionut.Stef@gmail.com>
2024-04-18 22:09:22 +0200

    Add Tag UpdatedAt field to the DbContext

---
+++ src/Demo.Core/Migrations/20240418200642_AddUpdatedAtToTags.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Demo.Core.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUpdatedAtToTags : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "updated_at",
+                table: "tags",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "0001-01-01 00:00:00");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "updated_at",
+                table: "tags");
+        }
+    }
+}
