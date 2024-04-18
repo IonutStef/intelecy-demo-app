5b15f3bd789b23946c3bb7546382ca2f1077ba4c
IonutStef <Dinu.Ionut.Stef@gmail.com>
2024-04-18 21:24:35 +0200

    Add UpdatedAt field to Tag

--- tests/Demo.Core.Tests/CQRS/Tags/UpdateTests.cs
+++ tests/Demo.Core.Tests/CQRS/Tags/UpdateTests.cs
@@ -1,13 +1,17 @@
 using Demo.Core.CQRS.Tags;
 using Demo.Core.Ids;
+using NodaTime;
+using NodaTime.Testing;
 
 namespace Demo.Core.Tests.CQRS.Tags;
 
 public class UpdateTests(Fixture fixture) : IClassFixture<Fixture>
 {
+    private static readonly Instant Time = Instant.FromUtc(2024, 4, 2, 13, 49, 0);
+
     private UpdateTagCommandHandler CreateHandler()
     {
-        return new UpdateTagCommandHandler(new TestDbContextFactory(fixture.Options));
+        return new UpdateTagCommandHandler(new TestDbContextFactory(fixture.Options), new FakeClock(Time));
     }
 
     [Fact]
@@ -35,6 +39,34 @@ public class UpdateTests(Fixture fixture) : IClassFixture<Fixture>
         // make sure others are changed
         Assert.Equal(newName, dbTag.Name);
         Assert.Equal(newUnit, dbTag.Unit);
+    }
+
+    [Fact]
+    public async Task Update_Should_SetUpdatedAt()
+    {
+        var handler = CreateHandler();
+
+        var id = TagId.From(fixture.Tags[2].Id);
+        const string newName = "sdfvdsfv";
+        const string newUnit = "dfvb";
+
+        var tag = await handler.Handle(new UpdateTagCommand(id, newName, newUnit), default);
+        // make sure some fields not changed
+        Assert.Equal(fixture.Sites[0].Id, tag.SiteId);
+        // make sure others are changed
+        Assert.Equal(newName, tag.Name);
+        Assert.Equal(newUnit, tag.Unit);
+
+        // fetch from DB
+        await using var context = new DemoDbContext(fixture.Options);
+        var dbTag = await context.Tags.FindAsync(id.Value);
+        Assert.NotNull(dbTag);
+        // make sure some fields not changed
+        Assert.Equal(fixture.Sites[0].Id, dbTag.SiteId);
+        // make sure others are changed
+        Assert.Equal(newName, dbTag.Name);
+        Assert.Equal(newUnit, dbTag.Unit);
+        Assert.Equal(Time, dbTag.UpdatedAt);
     }
 
     [Fact]
